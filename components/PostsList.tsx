cb5e3be7d3ae3bb756c48c0395c54074d8440afc
Krzysztof Pantak <51516375+Kielx@users.noreply.github.com>
2022-10-31 13:03:39 +0000

    Update styling, add date and location

--- components/PostsList.tsx
+++ components/PostsList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { VStack, Box, Divider, FavouriteIcon } from 'native-base';
+import { VStack, Box, Text, Divider, FavouriteIcon, Flex } from 'native-base';
 import db from '../firebaseConfig';
 
 import { collection, getDocs, DocumentData } from 'firebase/firestore';
@@ -11,7 +11,6 @@ export default function () {
     const getAnnouncements = async () => {
       const querySnapshot = await getDocs(collection(db, 'publicPosts'));
       querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data().title}`);
         setPosts((prev) => [...prev, doc.data()]);
       });
     };
@@ -20,49 +19,32 @@ export default function () {
 
   const mapPosts = posts.map((post) => {
     return (
-      <Box key={post.id} p="4" w="90%" mx="auto">
-        <VStack space={2}>
-          <Box
-            _light={{ bg: 'green.300', color: 'white' }}
-            _dark={{ bg: 'gray.900', color: 'white' }}
-            p="4"
-            rounded="md">
-            <Box>
-              <Box mt="1" fontWeight="bold" lineHeight="tight">
-                {post.title}
-              </Box>
-              <Box>{post.description}</Box>
-              <Box>
-              <FavouriteIcon size="5" mt="0.5" />
-              {post.likes}
-              </Box>
-            </Box>
-          </Box>
-        </VStack>
+      <Box
+        _light={{ bg: 'green.300', color: 'white' }}
+        _dark={{ bg: 'gray.900', color: 'white' }}
+        p="4"
+        rounded="md"
+        shadow="3">
+        <Box>
+          <Text pt="1" fontWeight="bold" fontSize="xl" lineHeight="tight">
+            {post.title}
+          </Text>
+          <Text fontSize="sm" color="gray.500">
+            {post.date.toDate().toLocaleDateString('pl-PL')} - {post.location}
+          </Text>
+          <Box py="1">{post.description}</Box>
+          <Flex flexDirection="row">
+            {post.likes}
+            <FavouriteIcon size="5" mt="0.5" color="red.700" ml="2" />
+          </Flex>
+        </Box>
       </Box>
     );
   });
 
   return (
-    <Box borderRadius="md">
+    <VStack borderRadius="md" width="1/4" space="4">
       {mapPosts}
-      <VStack
-        space="4"
-        divider={<Divider />}
-        width="100%"
-        _light={{ bg: 'gray.50', color: 'white' }}
-        _dark={{ bg: 'gray.700', color: 'white' }}>
-        <Box px="4" pt="4">
-          NativeBase
-        </Box>
-        <Box px="4">
-          NativeBase is a free and open source framework that enable developers to build
-          high-quality mobile apps using React Native iOS and Android apps with a fusion of ES6.
-        </Box>
-        <Box px="4" pb="4">
-          GeekyAnts
-        </Box>
-      </VStack>
-    </Box>
+    </VStack>
   );
 }
