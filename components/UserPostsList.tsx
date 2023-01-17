f0234f570d993d54369a258de0b72c9a9db587ba
Kielx <kiel66@gmail.com>
2023-01-17 11:21:35 +0100

    Update users posts listt with sorting capability

--- components/UserPostsList.tsx
+++ components/UserPostsList.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-array-index-key */
 import React, { useContext, useEffect, useState } from 'react';
-import { ScrollView } from 'native-base';
+import { Flex, ScrollView, Select, Text } from 'native-base';
 import { DocumentData } from 'firebase/firestore';
 import { AuthContext } from '../utils/AuthStateListener';
 import { verticalScale, horizontalScale } from '../utils/Metrics';
@@ -10,15 +10,26 @@ import { AppContext } from '../context/AppContext';
 
 export default function UserPostsList() {
   const { currentUser } = useContext(AuthContext);
-  const { posts, postsLoading } = useContext(AppContext);
+  const { posts, postsLoading, setPostsLoading } = useContext(AppContext);
   const [filteredPosts, setFilteredPosts] = useState<DocumentData[]>([]);
+  const [sortingCriteria, setSortingCriteria] = React.useState('userPosts');
 
   useEffect(() => {
+    setPostsLoading(true);
     const preFilteredPosts = posts.filter(
       (post: { authorId: string }) => post.authorId === currentUser?.uid
     );
-    setFilteredPosts(preFilteredPosts);
-  }, [posts]);
+    if (sortingCriteria === 'likedPosts') {
+      const likedPosts = preFilteredPosts.filter((post: DocumentData) =>
+        post.likes.includes(currentUser?.uid)
+      );
+      setFilteredPosts(likedPosts);
+    }
+    if (sortingCriteria === 'userPosts') {
+      setFilteredPosts(preFilteredPosts);
+    }
+    setPostsLoading(false);
+  }, [sortingCriteria, posts]);
 
   const mapPosts = filteredPosts.map((post: DocumentData) => (
     <PostShort post={post} key={post.id} />
@@ -26,7 +37,22 @@ export default function UserPostsList() {
 
   return (
     <ScrollView flex="1" maxW="768px" w={horizontalScale(350)} h={verticalScale(350)}>
-      {postsLoading ? [...Array(5)].map((_, i) => <UserPostListSkeleton key={i} />) : mapPosts}
+      <Flex direction="row" alignItems="center" justifyContent="flex-end" mb={2}>
+        <Text fontSize="sm" mr="2" color="gray.400">
+          Sortuj według:
+        </Text>
+        <Select
+          placeholder="Pokaż posty:"
+          selectedValue={sortingCriteria}
+          width={150}
+          onValueChange={(itemValue: string) => setSortingCriteria(itemValue)}>
+          <Select.Item label="Twoje posty" value="userPosts" />
+          <Select.Item label="Polubione posty" value="likedPosts" />
+        </Select>
+      </Flex>
+      {postsLoading || filteredPosts.length === 0
+        ? [...Array(5)].map((_, i) => <UserPostListSkeleton key={i} />)
+        : mapPosts}
     </ScrollView>
   );
 }
